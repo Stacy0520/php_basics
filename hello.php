1d4835366205ed6e1d99f88be33e3bb71ecbf995
_Tess Cee <stacymonica543@gmail.com>
2023-06-09 11:39:44 +0300

    php basics

---
+++ hello.php
@@ -0,0 +1,12 @@
+<h1>Hello am stacy</h1>
+<p>
+    <?php
+    print "hello.\n";
+    echo ("tjis is echo one");
+    print("this is echo one");
+    $answer=6*7;
+    echo "answer is $answer";
+    ?>
+    </p>
+
+    
